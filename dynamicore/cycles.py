98ef5b1340ea4da24fd05f93bc87ef17fb0f0b8c
luis25817fc-byte <luis.25817fc@gmail.com>
2026-06-30 20:14:58 -0600

    Update cycles.py

--- dynamicore/cycles.py
+++ dynamicore/cycles.py
@@ -1,27 +1,54 @@
 class CycleDetector:
-    def __init__(self, graph):
-        self.graph = graph
+    """
+    Detecta ciclos en un sistema funcional representado como lista de transiciones.
+    Ejemplo: system[i] = siguiente estado desde i
+    """
 
-    def detect(self):
-        visited = set()
-        cycles = []
+    def __init__(self, system):
+        self.system = system
+        self.n = len(system)
+        self.cycles = []
+        self.visited = set()
+        self._compute_cycles()
 
-        for start in self.graph.states():
-            if start in visited:
+    def _compute_cycles(self):
+        visited_global = set()
+
+        for start in range(self.n):
+            if start in visited_global:
                 continue
 
             path = []
-            index = {}
-            node = start
+            current = start
+            local_index = {}
 
-            while node not in index and node not in visited:
-                index[node] = len(path)
-                path.append(node)
-                node = self.graph.next_state(node)
+            while current not in local_index and current not in visited_global:
+                local_index[current] = len(path)
+                path.append(current)
+                current = self.system[current]
 
-            if node in index:
-                cycles.append(path[index[node]:])
+            # si encontramos ciclo
+            if current in local_index:
+                cycle_start = local_index[current]
+                cycle = path[cycle_start:]
+                self.cycles.append(cycle)
 
-            visited.update(path)
+            visited_global.update(path)
 
-        return cycles
+    def get_cycles(self):
+        return self.cycles
+
+    def cycle_count(self):
+        return len(self.cycles)
+
+    def largest_cycle(self):
+        if not self.cycles:
+            return []
+        return max(self.cycles, key=len)
+
+    def summary(self):
+        return {
+            "cycle_count": len(self.cycles),
+            "cycles": self.cycles,
+            "largest_cycle": self.largest_cycle()
+                }
