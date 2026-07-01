756b1ac65de1221c48709b7c57a4c1e17bea5369
luis25817fc-byte <luis.25817fc@gmail.com>
2026-07-01 14:38:56 -0600

    Delete dynamicore/core directory

--- dynamicore/core/metrics.py
+++
@@ -1,9 +0,0 @@
-class StructuralMetrics:
-    def coherence(self, basins, total):
-        if total == 0:
-            return 0.0
-
-        if not basins:
-            return 0.0
-
-        return max(basins.values()) / total
