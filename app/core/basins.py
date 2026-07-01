c71533d3572081e841398956ce5980281ca9ac05
luis25817fc-byte <luis.25817fc@gmail.com>
2026-07-01 15:30:06 -0600

    Update basins.py

--- app/core/basins.py
+++ app/core/basins.py
@@ -1,1 +1,12 @@
+class BasinAnalyzer:
+    def __init__(self, system, cycles):
+        self.system = system
+        self.cycles = cycles.compute()
 
+    def compute(self):
+        basins = {}
+
+        for c in self.cycles:
+            basins[tuple(c)] = len(c)
+
+        return basins
