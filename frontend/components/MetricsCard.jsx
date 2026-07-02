3bfbeb6aa1167b73e2d9f7cf58e3a7e847cc98ca
luis25817fc-byte <luis.25817fc@gmail.com>
2026-07-01 21:44:12 -0600

    Update MetricsCard.jsx

--- frontend/components/MetricsCard.jsx
+++ frontend/components/MetricsCard.jsx
@@ -1,1 +1,23 @@
+export default function MetricsCard({ title, value }) {
 
+    return (
+
+        <div className="metricCard">
+
+            <div className="metricTitle">
+
+                {title}
+
+            </div>
+
+            <div className="metricValue">
+
+                {value}
+
+            </div>
+
+        </div>
+
+    );
+
+}
