39eb6df6841e43c1874976e85d56e021404d0e84
luis25817fc-byte <luis.25817fc@gmail.com>
2026-07-01 21:43:05 -0600

    Update Navbar.jsx

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,1 +1,31 @@
+export default function Navbar() {
 
+    return (
+
+        <nav className="navbar">
+
+            <div className="logo">
+
+                <span className="gold">Dynami</span>
+
+                <span className="white">Core</span>
+
+            </div>
+
+            <div className="menu">
+
+                <a href="/">Dashboard</a>
+
+                <a href="/">API</a>
+
+                <a href="/">Documentation</a>
+
+                <a href="/">Pricing</a>
+
+            </div>
+
+        </nav>
+
+    );
+
+}
