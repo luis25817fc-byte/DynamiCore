8f741d19e9a64000cd06ac2108e7f39fa82b89e6
luis25817fc-byte <luis.25817fc@gmail.com>
2026-07-01 20:04:16 -0600

    Update auth.py

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,13 +1,16 @@
 from app.core.db import get_user
 
-def validate_key(api_key: str):
+def validate_user(api_key):
+
     user = get_user(api_key)
 
     if not user:
         return None
 
-    requests = user[2]
-    limit = user[3]
+    plan = user[2]
+    requests = user[3]
+
+    limit = 200 if plan == "free" else 100000
 
     if requests >= limit:
         return "LIMIT"
