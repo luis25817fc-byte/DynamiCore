3ff0332ebf0c8b87c7e2a68685e586a5228b5af9
luis25817fc-byte <luis.25817fc@gmail.com>
2026-07-09 16:48:28 +0000

    Fix metrics package exports

--- app/core/metrics/__init__.py
+++ app/core/metrics/__init__.py
@@ -1,2 +1,14 @@
 
 from .entropy import EntropyEngine
+from .coherence import CoherenceEngine
+from .dynamics import DynamicsEngine
+from .potential import PotentialEngine
+from .divergence import DivergenceEngine
+
+__all__ = [
+    "EntropyEngine",
+    "CoherenceEngine",
+    "DynamicsEngine",
+    "PotentialEngine",
+    "DivergenceEngine"
+]
