75e7b1984614b22956c0712cc5dbf0262a4cfa01
luis25817fc-byte <TU_EMAIL>
2026-07-12 14:04:28 +0000

    V6.2.4 Graph Evolution Predictor

--- app/core/graph_intelligence/__init__.py
+++ app/core/graph_intelligence/__init__.py
@@ -4,3 +4,5 @@ from .graph_delta import GraphDeltaEngine
 from .evolution_layer import EvolutionLayer
 
 from .pattern_engine import StructuralPatternEngine
+
+from .evolution_predictor import GraphEvolutionPredictor
