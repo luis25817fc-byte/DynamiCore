d3161089bebb5ff103700541372201a6748c21d7
luis25817fc-byte <luis.25817fc@gmail.com>
2026-08-01 16:59:56 +0000

    DLIS-080 cognitive factory migration

--- app/core/cognitive/registry.py
+++ app/core/cognitive/registry.py
@@ -1,40 +1,13 @@
-from .context_builder import ContextBuilder
-from .reasoning_engine import ReasoningEngine
-from .confidence_engine import ConfidenceEngine
-from .objective_manager import ObjectiveManager
-from .action_selector import ActionSelector
-from .feedback_integrator import FeedbackIntegrator
+from .cognitive_factory import CognitiveFactory
 
 
-class CognitiveRegistry:
+class CognitiveRegistry(CognitiveFactory):
     """
-    DynamiCore Cognitive Registry
-    V8.0
+    Compatibility wrapper.
+    DLIS-080 Cognitive Factory migration.
     """
 
     VERSION = "V8.0"
 
 
-    def build(self):
-
-        return {
-
-            "context_builder":
-                ContextBuilder(),
-
-            "reasoning_engine":
-                ReasoningEngine(),
-
-            "confidence_engine":
-                ConfidenceEngine(),
-
-            "objective_manager":
-                ObjectiveManager(),
-
-            "action_selector":
-                ActionSelector(),
-
-            "feedback_integrator":
-                FeedbackIntegrator()
-
-        }
+cognitive_registry = CognitiveRegistry()
